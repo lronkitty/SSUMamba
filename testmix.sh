ae2e1dee2ec8b3662af55e968df14334bc1305d3
lronkitty <ironkitty@hotmail.com>
2024-05-16 13:05:59 +0800

    commit on "main"

--- testmix.sh
+++ testmix.sh
@@ -1,4 +1,4 @@
 python main.py gpu_ids=\'0\' data.bs=1 mode=test model=ssumamba \
 noise=mixture test=icvl_mix test.b_size=16 \
-ckpt_path=/home/fugym/data_3/papers/paper5/mamba/data/trainings/mamba/UMamba_72844_icvl_icvl_mix_MixtureNoise-mixture_0.0003/ckpts/last.ckpt \
+ckpt_path=ssumamba_mix.ckps \
 test.save_raw=false 
